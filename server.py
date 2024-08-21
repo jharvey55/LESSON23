306a2fd2713c39542d4cb8ae28f23138714b637e
jharvey55 <j.b.harvey@columbia.edu>
2024-08-21 11:21:46 -0400

    feat: demonstrates basic server

--- server.py
+++ server.py
@@ -1,2 +1,13 @@
 from flask import Flask, render_template, request
 from weather import get_current_weather
+
+app = Flask(__name__) # makes app a Flask app
+
+# Routes
+@app.route('/') # home page
+@app.route('/index') # alias for home page
+def index(): # return something after each rotue
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000) # runs the app on localhost:5000
