b0698852318b974e7138f73e79afd25a5a31466d
jharvey55 <j.b.harvey@columbia.edu>
2024-08-21 12:26:07 -0400

    feat: adds weather route

--- server.py
+++ server.py
@@ -8,7 +8,20 @@ app = Flask(__name__) # makes app a Flask app
 @app.route('/') # home page
 @app.route('/index') # alias for home page
 def index(): # return something after each rotue
-    return "Hello, World!"
+    return render_template('index.html') # render index.html template
+
+@app.route('/weather')
+def get_weather():
+    city = request.args.get('city') # get city from user input
+    weather_data = get_current_weather(city) # pass in city data to weather_data
+    
+    return render_template('weather.html',              # Template to fill
+                           title=weather_data['name'],  # pass city name to template
+                           status=weather_data['weather'][0]['description'].capitalize(), # get status
+                           temp=f"{weather_data['main']['temp']:.1f}", # get temperature
+                           feels_like = f"{weather_data['main']['feels_like']:.1f}" # get feels like temperature
+                                   
+    ) # send weather data to template
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=5000) # runs the app on localhost:5000
