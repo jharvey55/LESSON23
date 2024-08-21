1f2eb62d13b411328560975d8e87c0012de7f652
jharvey55 <j.b.harvey@columbia.edu>
2024-08-21 11:31:04 -0400

    feat: updates server.py to use waitress

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from weather import get_current_weather
+from waitress import serve
 
 app = Flask(__name__) # makes app a Flask app
 
@@ -10,6 +11,7 @@ def index(): # return something after each rotue
     return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000) # runs the app on localhost:5000
+    # app.run(host='0.0.0.0', port=5000) # runs the app on localhost:5000
+    serve(app, host='0.0.0.0', port=5000) # serves using waitress now
     
     
